7f5e500bd03db40e94c05dc5c7a9a32f43a1a173
Adonai Ford-williams <adonai.fordwilliams@dcmail.ca>
2022-02-06 18:50:31 -0500

    Added Services page Content

--- Scripts/app.js
+++ Scripts/app.js
@@ -60,6 +60,18 @@
         let AdonaiP = document.getElementById("second");
         AdonaiP.before(MainParagraph);
 
+        // Alexis p About paragraph
+        let MainParagraph2 = document.createElement("p");
+        MainParagraph2.setAttribute("id", "MainParagraph2");
+        MainParagraph2.setAttribute("class", "AlexParagraph");
+        let AboutAlexis = "Hi Im Alexis Morales, Im a CPGM student at Durham College."; 
+        let FourthParagraphString = `${AboutAlexis}
+        I love to program with the coding languages: C++, Java, Python;`
+        MainParagraph2.textContent = FourthParagraphString;
+        MainContent.appendChild(MainParagraph2);
+        let AboutAlex = document.getElementById("second");
+        AboutAlex.before(MainParagraph2);
+
         // Adonai Resume Link
         let resume = document.createElement("p");
         resume.setAttribute("id", "AdonaiResume");
@@ -69,6 +81,16 @@
         MainContent.appendChild(resume);
         let AdonaiR = document.getElementById("second");
         AdonaiR.before(resume);
+
+        // Alex Resume Link
+        let alexresume = document.createElement("p");
+        alexresume.setAttribute("id", "AlexResume");
+        alexresume.setAttribute("class", "AlexResume");
+        let AlexLink = "https://www.linkedin.com/in/alexis-morales-a31261221/";
+        alexresume.innerHTML = '<a href = "'+ AlexLink + '">Alexis Resume';
+        MainContent.append(alexresume);
+        let AlexisM = document.getElementById("second");
+        AlexisM.before(alexresume);
 
         // Add Human Resources to Navbar
         AddHr();
@@ -82,7 +104,45 @@
     {
         console.log("Projects Page");
         // Entry point
-        let MainContent = document.getElementsByTagName("main")[0];   
+        let MainContent = document.getElementsByTagName("main")[0];  
+
+        // Favourite Projects
+        let ProjectsH3 = document.createElement("h3");
+        ProjectsH3.setAttribute("id", "ProjectsH3");
+        let AdParagraph = "Adonai & Alex's Favourite Projects: ";
+        ProjectsH3.textContent = AdParagraph;
+        MainContent.appendChild(ProjectsH3);
+        let ProjectsH = document.getElementById("row2");
+        ProjectsH.before(ProjectsH3);
+
+
+        // Favourite Projects Descriptions
+        let ProjectParagraph1 = document.createElement("p");
+        ProjectParagraph1.setAttribute("id", "ProjectParagraph1");
+        ProjectParagraph1.innerHTML = "We Made A Tic Tac Toe Game in First Year!";
+        MainContent.appendChild(ProjectParagraph1);
+
+        // Description 2
+        let ProjectParagraph2 = document.createElement("p");
+        ProjectParagraph2.setAttribute("id", "ProjectParagraph2");
+        ProjectParagraph2.innerHTML = "Every new language begins at Hello World!";
+        MainContent.appendChild(ProjectParagraph2);
+
+        // Description 3
+        let ProjectParagraph3 = document.createElement("p");
+        ProjectParagraph3.setAttribute("id", "ProjectParagraph3");
+        ProjectParagraph3.innerHTML = "We created a program that can make a virtual car using OOP Principles!";
+        MainContent.appendChild(ProjectParagraph3);
+
+        // Insert Descriptions
+        let img1 = document.getElementById("img1");
+        img1.before(ProjectParagraph1);
+
+        let img2 = document.getElementById("img2");
+        img2.before(ProjectParagraph2);
+
+        let img3 = document.getElementById("img3");
+        img3.before(ProjectParagraph3);
 
         // Add Human Resources to Navbar
         AddHr();
@@ -118,13 +178,41 @@
 
         let ImageDescriptions = document.createElement("p");
         ImageDescriptions.setAttribute("id", "ImageDescriptions");
-        let description1 = "Designed User Experience Flowchart";
-        ImageDescriptions.textContent = description1;
+        ImageDescriptions.innerHTML = "Designed a potential User Experience Flowchart&emsp;&emsp;"
+        + "&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;"
+        +"Designed a mock User Graphical Interface";
         MainContent.appendChild(ImageDescriptions);
 
+        let AlexDescriptions = document.createElement("p");
+        AlexDescriptions.setAttribute("id", "AlexDescriptions");
+        AlexDescriptions.innerHTML = "Professionally Editing Videos&emsp;&emsp;"
+        + "&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;"
+        +"&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;Program in any language";
+        MainContent.append(AlexDescriptions);
+
+        let AlexService = document.createElement("h3");
+        AlexService.setAttribute("id", "AlexService");
+        let AlexParagraph = "Alex's Top 3 Skills are: ";
+        AlexService.textContent = AlexParagraph;
+        MainContent.appendChild(AlexService);
+
+        let AlexList = document.createElement("ol");
+        AlexList.setAttribute("id", "AlexList");
+        AlexList.innerHTML = '<li>Web Design</li>'
+        +'<li>HTML, PHP, CSS, Java, Python, C++</li><li>Video Editing</li>';
+        MainContent.appendChild(AlexList);
+        
+        // Insert Items at Specific location
         let ServiceContent = document.getElementById("row1");
         AdService.after(ServiceContent);        
         AdList.after(ServiceContent);
+        let ServiceContent1 = document.getElementById("img5");
+        ServiceContent1.before(ImageDescriptions);
+
+        let AlexContent = document.getElementById("img5");
+        AlexContent.before(AlexService);
+        AlexContent.before(AlexList);
+        
 
         // Add Human Resources to Navbar
         AddHr();
@@ -177,6 +265,8 @@
         console.log("Contact Page");
         // Add Human Resources to Navbar
         AddNavBar();
+        // Add Human Resources to Navbar
+        AddHr();
 
         let sendButton = document.getElementById("sendButton");
         let subscribeCheckbox = document.getElementById("subscribeCheckbox");
@@ -190,8 +280,8 @@
                 if(contact.serialize())
                 {
                     let key = contact.FullName.substring(0,1) + Date.now();
-
                     localStorage.setItem(key, contact.serialize());
+                    let homeRedirect = setTimeout(redirects, 1000);
                 }
             }
         });
@@ -268,6 +358,11 @@
         let footer = '<div class="container-fluid"><a class="navbar-brand" href="#">&#169;Copyright 2022</a></div>';
         navBar.innerHTML = footer;
         MainContent.appendChild(navBar);
+    }
+
+    function redirects()
+    {
+        location.href = "index.html";
     }
 
     // Named Function
